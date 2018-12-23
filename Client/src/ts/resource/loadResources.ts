dc6bc0362b9dd67a04f7a44fbe698fc47476193e
Zach Barham <zachymach@live.com>
2018-12-23 11:28:56 +1000

    add loading icon to loaded resources

--- Client/src/ts/resource/loadResources.ts
+++ Client/src/ts/resource/loadResources.ts
@@ -1,12 +1,25 @@
 import ResourceClient from "./ResourceClient";
-import {Logger} from "../getLogger";
-
-const logger = Logger.create("resource/load");
+import LoadingIcon from "../displays/loading/LoadingIcon";
 
 export default function loadResources(client: ResourceClient) {
     const promises = [];
 
+    // add loading icons to all async-filled elements
+    document.querySelectorAll("[async-filled]").forEach(el => {
+        const expectedTime = parseInt(el.getAttribute("async-timeout")) || -1;
+        const loader = new LoadingIcon(expectedTime);
+
+        el.textContent = ""; // clear
+        el.appendChild(loader.container);
+    });
+
     document.querySelectorAll("[resource]").forEach(el => {
+        // add loading icon
+        const expectedTime = parseInt(el.getAttribute("async-timeout")) || -1;
+        const loader = new LoadingIcon(expectedTime);
+        el.textContent = "";
+        el.appendChild(loader.container);
+
         promises.push(async () => {
             const resource = el.getAttribute("resource");
 
